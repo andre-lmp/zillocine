f1de07173a511133a184b8f0f691f5a97c4a08cb
Vitor Hugo <Hugosilva55442@gmail.com>
2025-01-02 21:47:00 -0300

    refactor: re-create header carousel with embla carousel react

---
+++ src/components/headerSlides/index.tsx
@@ -0,0 +1,63 @@
+'use server';
+
+// Hook personalizado do TMDB com funções de busca de conteudo
+import useTmdbFetch from '@/components/hooks/tmdb/index';
+
+import React from 'react';
+
+// Interface de tipos para objetos retornados pela api do TMDB
+import { tmdbObjProps } from '@/components/contexts/tmdbContext/index';
+
+import dynamic from 'next/dynamic';
+const Carousel = dynamic(() => import('@/components/emblaCarousel/templates/header'), { ssr: false });
+
+import { handlePromise } from '@/components/utils/tmdbApiData/promise/index';
+import { checkAvailability } from '@/components/utils/tmdbApiData/availability';
+import { getContentId } from '@/components/utils/tmdbApiData/id';
+
+interface CarouselProps {
+    currentPage: string;
+};
+
+export default async function HeaderCarousel( props: CarouselProps ) {
+    
+    const contentData: tmdbObjProps[] = [];
+    const { 
+        fetchSeriesByIdList, 
+        fetchMoviesByIdList, 
+        fetchReleasedMovies, 
+        fetchPopularMovies,
+        fetchPopularSeries        
+    } = useTmdbFetch(); 
+
+    if ( props.currentPage === 'home' ) {
+        const releasedMovies = await handlePromise(fetchReleasedMovies());
+        const selectedIds = await getContentId( releasedMovies );
+        const movies = await fetchMoviesByIdList( selectedIds );
+        const filteredContent = await checkAvailability( movies );
+        contentData.push( ...filteredContent );
+    };
+    
+    if ( props.currentPage === 'movies' ) {
+        const popularMovies = await handlePromise(fetchPopularMovies());
+        const selectedIds = await getContentId( popularMovies );
+        const movies = await fetchMoviesByIdList( selectedIds );
+        const filteredContent = await checkAvailability( movies );
+        contentData.push( ...filteredContent );
+    };
+
+    if ( props.currentPage === 'series' ) {
+        const popularSeries = await handlePromise(fetchPopularSeries());
+        const selectedIds = await getContentId( popularSeries );
+        const series = await fetchSeriesByIdList( selectedIds )
+        const filteredContent = await checkAvailability( series );
+        contentData.push( ...filteredContent );
+    };
+
+    return contentData.length ? (
+           <Carousel contentData={contentData} currentPage={props.currentPage}/>
+    ) : null
+};
+
+
+
--- src/components/headerSlides/introPoster/index.tsx
+++ src/components/headerSlides/introPoster/index.tsx
@@ -8,7 +8,7 @@ export default function IntroPoster() {
     const router = useRouter();
 
     return (
-        <div className="w-screen h-[660px] md:h-[400px] xl:h-[420px] overflow-hidden relative">
+        <div className="w-screen h-[660px] md:h-[420px] xl:h-[450px] overflow-hidden relative">
             <LazyLoadImage
                 src={'/intro_poster_high.png'}
                 alt={'image of a list of movies from the Zillocine website'}
@@ -20,18 +20,18 @@ export default function IntroPoster() {
             <div className='absolute w-full h-full z-[3] top-0 left-0 bg-gradient-to-b from-deepnight via-deepnight/50 to-deepnight'></div>
 
             <div className='w-full px-4 max-w-md sm:max-w-lg z-[5] xl:max-w-2xl absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 '>
-                <h1 className='text-[32px] sm:text-4xl xl:text-[45px] font-bold font-raleway text-center text-nowra leading-snug md:leading-[45px] lg:leading-[50px]'>Descubra tudo sobre seus filmes e séries favoritos <br></br>em um só lugar!</h1>
+                <h1 className='text-[32px] sm:text-4xl xl:text-[45px] font-bold font-raleway text-center text-nowra leading-tight md:leading-[45px] lg:leading-[50px]'>Descubra tudo sobre seus filmes e séries favoritos <br></br>em um só lugar!</h1>
 
                 <div className='w-full flex flex-col-reverse gap-5 justify-center items-center mt-10'>
                     <button onClick={() => router.push('movies')} 
-                        className='btn px-16 h-12 rounded-lg border bg-transparent hover:bg-transparent border-neutral-300 hover:border-neutral-300 text-white text-base font-medium outline-none'
+                        className='btn px-16 h-14 border bg-transparent hover:bg-transparent border-neutral-300 hover:border-neutral-300 text-white text-base font-medium outline-none rounded-full'
                         >
                         Explorar filmes
                     </button>
                     
                     <button 
                         onClick={() => router.push('series')} 
-                        className='btn px-16 h-12 rounded-lg border-none bg-darkslateblue hover:bg-darkslateblue text-white text-base font-medium outline-none'
+                        className='btn px-16 h-14 border-none bg-darkslateblue hover:bg-darkslateblue text-white text-base font-medium outline-none rounded-full'
                         >
                         Explorar series
                     </button>
